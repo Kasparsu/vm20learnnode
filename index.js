6d79567109ce57fb4601ad382b89f8d122fee37d
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-28 11:21:31 +0300

    add boxen and chalk examples

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let calc = require('./calc.js');
+const chalk = require('chalk');
+const boxen = require('boxen');
+console.log(chalk.bgYellow.red('Hello'));
 
-let answer = calc.add(3,5);
-console.log(answer);
+let options = {
+    padding: 1,
+    borderStyle: 'round',
+    title: 'hello',
+    titleAlignment: 'center',
+    backgroundColor: 'red'
+}
+
+
+console.log(boxen('unicorn', options));
+console.log(boxen(chalk.underline('unicorn'), options));
