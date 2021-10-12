3b7a87cf8f1d07c2b0e53abe61cb4ceb2e81ef5c
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-10-12 10:26:18 +0300

    Add vue

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const chalk = require('chalk');
-const boxen = require('boxen');
-console.log(chalk.bgYellow.red('Hello'));
-
-let options = {
-    padding: 1,
-    borderStyle: 'round',
-    title: 'hello',
-    titleAlignment: 'center',
-    backgroundColor: 'red'
-}
-
-
-console.log(boxen('unicorn', options));
-console.log(boxen(chalk.underline('unicorn'), options));
+let Vue = require('vue');
